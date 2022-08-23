91a59229d46556e944cbeb03f42dc0d4dfee77d9
Hugo <hjcarranzaarroyo@hotmail.com>
2022-08-22 22:50:02 -0600

    Interfaz y csv

---
+++ Extraclases/src/main/java/com/mycompany/extraclases/Estudiantes.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.extraclases;
+
+/**
+ *
+ * @author hjcar
+ */
+public class Estudiantes {
+    public class estudiante {
+    private int carnet;
+    private String nombre;
+    private String email;
+    private int telefono;
+    private String nickname;
+    private String tipo;
+    private int nota_examen;
+    private int nota_quiz;
+    private int nota_tarea;
+    private int proyecto1;
+    private int proyecto2;
+    private int proyecto3;
+
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNota_examen() {
+        return nota_examen;
+    }
+
+    public void setNota_examen(int nota_examen) {
+        this.nota_examen = nota_examen;
+    }
+
+    public int getNota_quiz() {
+        return nota_quiz;
+    }
+
+    public void setNota_quiz(int nota_quiz) {
+        this.nota_quiz = nota_quiz;
+    }
+
+    public int getNota_tarea() {
+        return nota_tarea;
+    }
+
+    public void setNota_tarea(int nota_tarea) {
+        this.nota_tarea = nota_tarea;
+    }
+
+    public int getProyecto1() {
+        return proyecto1;
+    }
+
+    public void setProyecto1(int proyecto1) {
+        this.proyecto1 = proyecto1;
+    }
+
+    public int getProyecto2() {
+        return proyecto2;
+    }
+
+    public void setProyecto2(int proyecto2) {
+        this.proyecto2 = proyecto2;
+    }
+
+    public int getProyecto3() {
+        return proyecto3;
+    }
+
+    public void setProyecto3(int proyecto3) {
+        this.proyecto3 = proyecto3;
+    }
+}
+
+}
